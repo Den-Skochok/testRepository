fd40d859c73c66de6293315451d630afe2831a14
Den-Skochok <denis93skochok@gmail.com>
2022-10-21 07:52:55 +0300

    test

--- testRepository/src/test/java/web/tests/Tests/TestTest.java
+++ testRepository/src/test/java/web/tests/Tests/TestTest.java
@@ -6,7 +6,7 @@ import web.tests.model.Page;
 public class TestTest extends TestBase {
 
     @Test
-    public void goToPageTests() throws InterruptedException {
+    public void goToPageTest() throws InterruptedException {
         applicationManager.getPageNavigationHelper().goToPage(new Page("https://app3.signnow-dev.xyz/webapp/documents/1757a5bdff0042bf8ebc67b617f9106a0e0a3cb6"));
     }
 
